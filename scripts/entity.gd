390ddc9526771844315a904505ee81914102a7cd
jacevihzalel <jacevihzalel@gmail.com>
2025-06-12 07:03:27 +0800

    Changed file parent child relationships and added new inhereted scene 'entitiy'

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -7,7 +7,9 @@ var wobble_time_elapsed := 0.0
 var wobble_speed := 10.0
 var degree_of_wobble := 5.0
 
-var mobs # To store instances of mobs in group "mobs"
+var attack_time_elapsed := 0.0
+
+var opposing_entities # To store instances of opposing_entities
 	
 func _process(delta):
 	# Play wobble animation when character is moving
@@ -22,9 +24,13 @@ func _process(delta):
 	else:
 		$Sprite2D.flip_h = 0
 	
-	mobs = get_tree().get_nodes_in_group("mobs") # Store instances of mobs in group "mobs"
-	var target = get_closest_mob() # Find the closest mob from the mobs array
-	# Walk towards mob if there is a mob
+	if is_in_group("adventurers"):
+		opposing_entities = get_tree().get_nodes_in_group("mobs") # Store instances of opposing_entities
+	elif is_in_group("mobs"):
+		opposing_entities = get_tree().get_nodes_in_group("adventurers")
+	
+	var target = get_closest_opposing_entity() # Find the closest opposing_entity from the mobs array
+	# Walk towards opposing_entity if there is one
 	if target:
 		var direction = (target.global_position - global_position).normalized()
 		velocity = direction * speed
@@ -33,17 +39,17 @@ func _process(delta):
 		velocity = Vector2.ZERO
 		move_and_slide()
 
-func get_closest_mob():
+func get_closest_opposing_entity():
 	var closest_distance = INF # Set to infinity 
-	var closest_mob
+	var closest_opposing_entity
 	
-	if mobs.is_empty():
+	if opposing_entities.is_empty():
 		return null
 	else:
 		# Compare each distance from a mob and return the closest mob
-		for mob in mobs:
-			var distance = global_position.distance_to(mob.global_position)
+		for opposing_entity in opposing_entities:
+			var distance = global_position.distance_to(opposing_entity.global_position)
 			if (distance < closest_distance):
 				closest_distance = distance
-				closest_mob = mob
-		return closest_mob
+				closest_opposing_entity = opposing_entity
+		return closest_opposing_entity

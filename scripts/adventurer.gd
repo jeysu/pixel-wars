8efd9feec8f381555e28e45eef8dd4a0cc4de705
jacevihzalel <jacevihzalel@gmail.com>
2025-06-12 04:55:22 +0800

    Added wobbling animation and flip direction

--- scripts/adventurer.gd
+++ scripts/adventurer.gd
@@ -1,11 +1,30 @@
 extends CharacterBody2D
 
-var speed = 100
-var mobs
+var speed = 100 # Speed of character
+
+# Factors for wobble animation
+var wobble_time_elapsed := 0.0
+var wobble_speed := 10.0
+var degree_of_wobble := 5.0
+
+var mobs # To store instances of mobs in group "mobs"
 	
 func _process(delta):
-	mobs = get_tree().get_nodes_in_group("mobs")
-	var target = get_closest_mob()
+	# Play wobble animation when character is moving
+	if velocity.length() > 0:
+		wobble_time_elapsed += delta * wobble_speed
+		wobble_time_elapsed = wrapf(wobble_time_elapsed, 0, TAU) # Reset wobble_time_elapsed to 0 after sin cycle
+		$Sprite2D.rotation_degrees = sin(wobble_time_elapsed) * degree_of_wobble # Smooth transition using sin wave
+	
+	# Flip sprite depending on direction
+	if velocity.x > 0:
+		$Sprite2D.flip_h = 1
+	else:
+		$Sprite2D.flip_h = 0
+	
+	mobs = get_tree().get_nodes_in_group("mobs") # Store instances of mobs in group "mobs"
+	var target = get_closest_mob() # Find the closest mob from the mobs array
+	# Walk towards mob if there is a mob
 	if target:
 		var direction = (target.global_position - global_position).normalized()
 		velocity = direction * speed
@@ -15,12 +34,13 @@ func _process(delta):
 		move_and_slide()
 
 func get_closest_mob():
-	var closest_distance = INF
+	var closest_distance = INF # Set to infinity 
 	var closest_mob
 	
 	if mobs.is_empty():
 		return null
 	else:
+		# Compare each distance from a mob and return the closest mob
 		for mob in mobs:
 			var distance = global_position.distance_to(mob.global_position)
 			if (distance < closest_distance):
--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
 
-var speed = 100
-var adventurers
-	
+var speed = 100 # Speed of character
+
+# Factors for wobble animation
+var wobble_time_elapsed := 0.0
+var wobble_speed := 10.0
+var degree_of_wobble := 5.0
+
+var adventurers # To store instances of adventurers in group "adventurers"
+
 func _process(delta):
-	adventurers = get_tree().get_nodes_in_group("adventurers")
-	var target = get_closest_adventurer()
+	# Play wobble animation when character is moving
+	if velocity.length() > 0:
+		wobble_time_elapsed += fmod(delta * wobble_speed, TAU) # Reset wobble_time_elapsed to 0 after sin cycle
+		$Sprite2D.rotation_degrees = sin(wobble_time_elapsed) * degree_of_wobble # Smooth transition using sin wave
+	
+	# Flip sprite depending on direction
+	if velocity.x > 0:
+		$Sprite2D.flip_h = 1
+	else:
+		$Sprite2D.flip_h = 0
+	
+	adventurers = get_tree().get_nodes_in_group("adventurers") # Store instances of adventurers in group "adventurers"
+	var target = get_closest_adventurer() # Find the closest mob from the mobs array
+	# Walk towards adventurer if there is an adventurer
 	if target:
 		var direction = (target.global_position - global_position).normalized()
 		velocity = direction * speed
@@ -15,12 +33,13 @@ func _process(delta):
 		move_and_slide()
 
 func get_closest_adventurer():
-	var closest_distance = INF
+	var closest_distance = INF # Set to infinity
 	var closest_adventurer
 	
 	if adventurers.is_empty():
 		return null
 	else:
+		# Compare each distance from an adventurer and return the closest adventurer
 		for adventurer in adventurers:
 			var distance = global_position.distance_to(adventurer.global_position)
 			if (distance < closest_distance):

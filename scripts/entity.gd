47d9bc2f332ec5a211b564db63a3b86c3ff554f3
jacevihzalel <jacevihzalel@gmail.com>
2025-06-13 00:31:33 +0800

    Added attack event when opposing groups collide (adventurer vs mob)

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -1,55 +1,77 @@
 extends CharacterBody2D
 
-var speed = 100 # Speed of character
+@export var hp := 100
+@export var speed := 100
+@export var attack := 10
+@export var attack_speed := 1
 
-# Factors for wobble animation
 var wobble_time_elapsed := 0.0
 var wobble_speed := 10.0
 var degree_of_wobble := 5.0
-
 var attack_time_elapsed := 0.0
 
-var opposing_entities # To store instances of opposing_entities
+var opposing_entities
+var target
+var collision
 	
 func _process(delta):
-	# Play wobble animation when character is moving
+	get_opposing_entities()
+	move_to_target(delta)
+	play_walking_animation(delta)
+	flip_sprite()
+	
+	if collision:
+		var collided_node = collision.get_collider()
+		if collided_node in opposing_entities:
+			apply_attack(collided_node, delta)
+			
+func get_opposing_entities():
+	if is_in_group("adventurers"):
+		opposing_entities = get_tree().get_nodes_in_group("mobs")
+	elif is_in_group("mobs"):
+		opposing_entities = get_tree().get_nodes_in_group("adventurers")
+
+func move_to_target(delta):
+	target = get_closest_opposing_entity()
+	if target:
+		var direction = (target.global_position - global_position).normalized()
+		velocity = direction * speed * delta
+	else:
+		velocity = Vector2.ZERO * delta
+	collision = move_and_collide(velocity)
+
+func play_walking_animation(delta):
 	if velocity.length() > 0:
 		wobble_time_elapsed += delta * wobble_speed
 		wobble_time_elapsed = wrapf(wobble_time_elapsed, 0, TAU) # Reset wobble_time_elapsed to 0 after sin cycle
 		$Sprite2D.rotation_degrees = sin(wobble_time_elapsed) * degree_of_wobble # Smooth transition using sin wave
-	
-	# Flip sprite depending on direction
+
+func flip_sprite():
 	if velocity.x > 0:
 		$Sprite2D.flip_h = 1
 	else:
 		$Sprite2D.flip_h = 0
 	
-	if is_in_group("adventurers"):
-		opposing_entities = get_tree().get_nodes_in_group("mobs") # Store instances of opposing_entities
-	elif is_in_group("mobs"):
-		opposing_entities = get_tree().get_nodes_in_group("adventurers") # Store instances of opposing_entities
-	
-	var target = get_closest_opposing_entity() # Find the closest opposing_entity from the mobs array
-	# Walk towards opposing_entity if there is one
-	if target:
-		var direction = (target.global_position - global_position).normalized()
-		velocity = direction * speed
-		move_and_slide()
-	else:
-		velocity = Vector2.ZERO
-		move_and_slide()
-
 func get_closest_opposing_entity():
-	var closest_distance = INF # Set to infinity 
+	var closest_distance = INF
 	var closest_opposing_entity
 	
 	if opposing_entities.is_empty():
 		return null
-	else:
-		# Compare each distance from a mob and return the closest mob
-		for opposing_entity in opposing_entities:
-			var distance = global_position.distance_to(opposing_entity.global_position)
-			if (distance < closest_distance):
-				closest_distance = distance
-				closest_opposing_entity = opposing_entity
-		return closest_opposing_entity
+	# Compare each distance from a mob and return the closest mob
+	for opposing_entity in opposing_entities:
+		var distance = global_position.distance_to(opposing_entity.global_position)
+		if (distance < closest_distance):
+			closest_distance = distance
+			closest_opposing_entity = opposing_entity
+	return closest_opposing_entity
+
+func apply_attack(enemy, delta):
+	attack_time_elapsed += delta
+	if attack_time_elapsed >= 1 / attack_speed:
+		enemy.take_damage(attack)
+
+func take_damage(attack):
+	hp -= attack
+	if hp <= 0:
+		queue_free()
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,23 +3,20 @@ extends Node
 @export var adventurers: PackedScene
 @export var mobs: PackedScene
 
-# Number of entities
 var number_of_adventurers = 10
 var number_of_mobs = 5
 
 func _ready():
-	# Instantiate a mob depending on number_of_mobs
 	for n in number_of_mobs:
 		var mob = mobs.instantiate()
 		var mob_spawn_location = $MobSpawnPath/SpawnPoint
-		mob_spawn_location.progress_ratio = randf() # Randomize spawn
+		mob_spawn_location.progress_ratio = randf()
 		mob.position = mob_spawn_location.position
 		add_child(mob)
 	
-	# Instantiate an adventurer depending on number_of_adventurers
 	for n in number_of_adventurers:
 		var adventurer = adventurers.instantiate()
 		var adventurer_spawn_location = $AdventurerSpawnPath/SpawnPoint
-		adventurer_spawn_location.progress_ratio = randf() # Randomize spawn
+		adventurer_spawn_location.progress_ratio = randf()
 		adventurer.position = adventurer_spawn_location.position
 		add_child(adventurer)
